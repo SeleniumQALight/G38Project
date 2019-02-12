7094de11181a7d618fc042e05dee9ec58fbffc37
ViktoriyaGolubeva <komloboda@gmail.com>
2019-02-12 20:56:36 +0200

    test commit

--- golubevaProj/src/test/java/mainPack/AppTest.java
+++ golubevaProj/src/test/java/mainPack/AppTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     * Rigorous Test :-) hello it's my change
      */
     @Test
     public void shouldAnswerWithTrue()
