6f0efef1315b4561ba7bcc45f568523d1f8de2c9
ViktoriyaGolubeva <komloboda@gmail.com>
2019-02-12 20:59:13 +0200

    test commit 2

--- golubevaProj/src/test/java/mainPack/AppTest.java
+++ golubevaProj/src/test/java/mainPack/AppTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App. hi work
  */
 public class AppTest 
 {
     /**
-     * Rigorous Test :-) hello it's my change
+     * Rigorous Test :-) hello it's my change. My change 2
      */
     @Test
     public void shouldAnswerWithTrue()
