2cb094cbce500e8de83490801aaf3cc32518b0af
bulatetskyi_a <k2300988bav@gmail.com>
2019-02-12 20:59:25 +0200

    dopisal bla

--- bulatetskiiProj/src/test/java/mainPack/AppTest.java
+++ bulatetskiiProj/src/test/java/mainPack/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App. bla bla bla
+ * Unit test for simple App. bla bla bla bla
  */
 public class AppTest 
 {
