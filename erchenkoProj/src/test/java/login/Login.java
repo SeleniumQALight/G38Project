fb0b7ae55b8cec40de84b4e004ac1d288bdbbdef
belgo <belgo.emv@gmail.com>
2019-02-20 20:36:50 +0200

    first test + invalidLogin

--- erchenkoProj/src/test/java/login/Login.java
+++ erchenkoProj/src/test/java/login/Login.java
@@ -31,4 +31,24 @@ public class Login {
         Assert.assertTrue("Avatar is not present", webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
         webDriver.quit();
     }
+
+    @Test
+    public void invalidLogin () {
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("http://v3.test.itpmgroup.com/");
+        webDriver.findElement(By.name("_username")).sendKeys("St609");
+        webDriver.findElement(By.id("password")).sendKeys("909090");
+        webDriver.findElement(By.tagName("button")).click();
+
+        Assert.assertTrue("Registration form is not present", webDriver.findElement(By.className("login-box-msg")).isDisplayed());
+
+        webDriver.quit();
+    }
 }
