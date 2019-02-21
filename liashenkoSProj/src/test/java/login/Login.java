53181653678505597e4a20a2226832bcf67849dc
SergeyLashenco <sergeylashenco@gmail.com>
2019-02-21 18:43:04 +0200

    Test notValidLogin

--- liashenkoSProj/src/test/java/login/Login.java
+++ liashenkoSProj/src/test/java/login/Login.java
@@ -27,9 +27,36 @@ public class Login {
         webDriver.findElement(By.id("password")).sendKeys("909090");
         webDriver.findElement(By.tagName("button")).click();
 
-        Assert.assertTrue("Avatar is not present",webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+        Assert.assertTrue("Avatar is not present",
+                webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
 
         webDriver.quit();
 
     }
+
+    @Test
+    public void notValidLogIn() {
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get("http://v3.test.itpmgroup.com");
+
+        System.out.println("Not Authorization on Site");
+
+        webDriver.findElement(By.name("_username")).sendKeys("Studenttt");
+        webDriver.findElement(By.id("password")).sendKeys("909090");
+        webDriver.findElement(By.tagName("button")).click();
+
+
+        Assert.assertTrue("Not Authorization",
+                webDriver.findElement(By.xpath(".//input[@name='_username']")).isDisplayed());
+
+        webDriver.quit();
+
+    }
+
 }
