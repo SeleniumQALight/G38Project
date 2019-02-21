1e075d1b6415728eef2cd54d8cbad13de6398e39
iboruk <igor.boruk@gmail.com>
2019-02-21 21:41:03 +0200

    first test with ObjectPage

--- borukProj/src/test/java/login/Login.java
+++ borukProj/src/test/java/login/Login.java
@@ -12,7 +12,27 @@ import java.util.concurrent.TimeUnit;
 public class Login {
     @Test
     public void validLogIn() {
+        WebDriver webDriver = new ChromeDriver(); // create object for chrome
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webDriver.chrome.driver", file.getAbsolutePath());
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("http://v3.test.itpmgroup.com");
+        System.out.println("Site was opened");
+
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+        webDriver.findElement(By.id("password")).sendKeys("909090");
+        webDriver.findElement(By.tagName("button")).click();
+
+        Assert.assertTrue("Avatar is not presented", webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+        webDriver.quit();
+    }
+    @Test
+    public void inValidLogIn() {
         WebDriver webdriver = new ChromeDriver(); // create object for chrome
+
 //        File file = new File("./src/drivers/chromedriver.exe");
 //        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
@@ -22,7 +42,7 @@ public class Login {
         webdriver.get("http://v3.test.itpmgroup.com");
         System.out.println("Site was opened");
 
-        webdriver.findElement(By.name("_username")).sendKeys("Student");
+        webdriver.findElement(By.name("_username")).sendKeys("Studentka");
         webdriver.findElement(By.id("password")).sendKeys("909090");
         webdriver.findElement(By.tagName("button")).click();
 
