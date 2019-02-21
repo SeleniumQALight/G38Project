0fd2e26efa9ae624869de6b9be8f9430f084838b
Alexandr <kuznietsovv@gmail.com>
2019-02-21 21:41:08 +0200

    first test with PageObject

---
+++ kusnetsovProj/src/test/java/login/LoginWithPageObject.java
@@ -0,0 +1,17 @@
+package login;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class LoginWithPageObject extends ParentTest {
+    @Test
+    public void validLogin () {
+    loginPage.openPage();
+    loginPage.enterTextInToInputLogin("Student");
+    loginPage.enterTextInToInputPass("909090");
+    loginPage.clickOnButtonSubmit();
+
+    checkExpectedResult ("Avatar is not present",homePage.isAvatarPresent());
+
+    }
+}
