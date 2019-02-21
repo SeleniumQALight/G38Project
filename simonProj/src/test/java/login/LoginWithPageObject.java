b787795ae7cf235fe4ebf66ae5365f16c9aeaddd
Pavel Simon <pavelsimonua@gmail.com>
2019-02-21 21:40:15 +0200

    First test with PO

---
+++ simonProj/src/test/java/login/LoginWithPageObject.java
@@ -0,0 +1,21 @@
+package login;
+
+import org.junit.Test;
+import parenttest.Parenttest;
+
+public class LoginWithPageObject extends Parenttest {
+    @Test
+    public void validLogin() {
+
+        loginPage.openPage();
+        loginPage.enterTextInToInputLogin("Student");
+        loginPage.enterTextInToInputPass("909090");
+        loginPage.clickOnButtonSubmit();
+
+        checkExpectedResult("Avatar is not present", homePage.isAvatarPresent());
+
+
+        }
+
+
+}
