3b7397ec67cee22ac3fbd3fe5e265e24e710a38e
ievgen <edukhnin@gmail.com>
2019-02-24 17:52:45 +0200

    The first test with page object

---
+++ duhinProj/src/main/java/pages/ParentPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
+public class ParentPage {
+    WebDriver webDriver;
+
+    public ParentPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+}
