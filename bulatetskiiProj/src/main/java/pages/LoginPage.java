475cc549cce58cccf1f410ea59c97ec89abe61c0
bulatetskyi_a <k2300988bav@gmail.com>
2019-02-26 21:41:40 +0200

    bulatetsyi-26-02

--- bulatetskiiProj/src/main/java/pages/LoginPage.java
+++ bulatetskiiProj/src/main/java/pages/LoginPage.java
@@ -1,6 +1,48 @@
 package pages;
 
-public class LoginPage {
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends ParentPage{
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+    @FindBy (name = "_username")
+    private WebElement inputLogin;
+
+    @FindBy (id = "password")
+    private WebElement inputPass;
+
+    @FindBy (tagName = "button")
+    private  WebElement button;
+
+    public void openPage() {
+        try{
+            webDriver.get("http://v3.test.itpmgroup.com");
+            System.out.println("Login page was opened");
+        } catch (Exception e){
+            System.out.println("Can not open Login page " + e); // for logger
+            Assert.fail("Can not open Login page " + e); // for report
+        }
+    }
 
 
+    public void enterTextInToInputLogin(String login) {
+
+         actionsWithOurElements.enterTextInToElement(inputLogin, login);
+    }
+
+
+    public void enterTextInToInputPass(String pass) {
+         actionsWithOurElements.enterTextInToElement(inputPass, pass);
+    }
+
+
+    public void clickOnButtonSubmit (){
+         actionsWithOurElements.clickOnElement (button);
+    }
 }
