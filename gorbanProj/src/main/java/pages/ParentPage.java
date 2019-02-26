c79494bae4fbf17944a55d3f7339bc4e715ef643
srgorban <46221132+srgorban@users.noreply.github.com>
2019-02-26 11:14:34 +0200

    Move ParentPage for new Branch

--- gorbanProj/src/main/java/pages/ParentPage.java
+++ gorbanProj/src/main/java/pages/ParentPage.java
@@ -1,4 +1,12 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class ParentPage {
+    WebDriver webDriver;
+
+
+    public ParentPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
 }
