776c76856e50619ac4b705fb4878df01dfa38c95
srgorban <46221132+srgorban@users.noreply.github.com>
2019-02-26 10:29:14 +0200

    Move Login for new Branch

--- gorbanProj/src/test/java/login/Login.java
+++ gorbanProj/src/test/java/login/Login.java
@@ -1,4 +1,34 @@
 package login;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class Login {
+
+    @Test
+    public void validLogin() {
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("http://v3.test.itpmgroup.com");
+        System.out.println("Site was opened");
+
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+        webDriver.findElement(By.id("password")).sendKeys("909090");
+        webDriver.findElement(By.tagName("button")).click();
+        Assert.assertTrue("Avatar is not present",webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+
+        webDriver.quit();
+    }
 }
