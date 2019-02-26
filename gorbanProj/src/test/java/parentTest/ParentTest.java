ac8cd087035bb88b71ad3e93563e47276e8ce83f
srgorban <46221132+srgorban@users.noreply.github.com>
2019-02-26 09:33:48 +0200

    test Parent

--- gorbanProj/src/test/java/parentTest/ParentTest.java
+++ gorbanProj/src/test/java/parentTest/ParentTest.java
@@ -1,4 +1,12 @@
-package parentTest;
+package pages;
 
-public class ParentTest {
+import org.openqa.selenium.WebDriver;
+
+public class ParentPage {
+    WebDriver webDriver;
+
+
+    public ParentPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
 }
