b64324e54938ceed2c8f5beebbb4c0d58c9518a6
Aleksandr <chepik2058@gmail.com>
2019-02-28 21:31:11 +0200

    28.02.19

--- chepikProj/src/main/java/libs/ActionsWithOurElements.java
+++ chepikProj/src/main/java/libs/ActionsWithOurElements.java
@@ -1,9 +1,11 @@
 package libs;
 
+
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
@@ -16,7 +18,7 @@ public class ActionsWithOurElements {
         try {
             element.clear();
             element.sendKeys(text);
-            System.out.println(text + "was input into element ");
+            logger.info(text + "was input into element ");
         }catch (Exception e) {
             printErrorAndStopTest (e);
         }
@@ -30,8 +32,36 @@ public class ActionsWithOurElements {
     public void clickOnElement(WebElement element) {
         try {
             element.click();
-            System.out.printf("Element was clicked");
+            logger.info("Element was clicked");
         }catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+    public boolean isElementPresent (WebElement webElement) {
+        try {
+            boolean isDisplayed = webElement.isDisplayed();
+            logger.info("Element is displayed -> " + isDisplayed);
+           return isDisplayed;
+        }catch (Exception e) {
+            logger.info("Element is displayed -> false");
+            return false;
+        }
+    }
+    public void selectTextInDD(WebElement element, String text) {
+        try {
+            Select select = new Select(element);
+            select.selectByVisibleText(text);
+            logger.info(text + " was seleected in DD");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+    public void selectValueInDD(WebElement element, String value) {
+        try {
+            Select select = new Select(element);
+            select.selectByValue(value);
+            logger.info(value + " was seleected in DD");
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
