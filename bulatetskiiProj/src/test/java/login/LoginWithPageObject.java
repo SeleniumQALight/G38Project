d86d6df380e0780552b94ef1b2aad10113a19b83
bulatetskyi_a <k2300988bav@gmail.com>
2019-02-28 21:31:20 +0200

    bulatetsyi-28-02

--- bulatetskiiProj/src/test/java/login/LoginWithPageObject.java
+++ bulatetskiiProj/src/test/java/login/LoginWithPageObject.java
@@ -1,13 +1,28 @@
 package login;
 
 import org.junit.Test;
-import parentTest.ParestTast;
+import parentTest.ParentTest;
 
-public class LoginWithPageObject extends ParestTast{
+
+
+public class LoginWithPageObject extends ParentTest {
+
     @Test
-    public void velidLogin (){
+    public void validLogin() {
+        loginPage.openPage();
+        loginPage.enterTextInToInputLogin("Student");
+        loginPage.enterTextInToInputPass("909090");
+        loginPage.clickOnButtonSubmit();
 
-
+        checkExpectedResult (
+                "Avatar is not present",
+                homePage.isAvatarPresent()
+        );
     }
 
+    @Test
+    public void invalidLogin (){
+        loginPage.login("fdsafas", "909090");
+        checkExpectedResult("Avatar should not  be preent", !homePage.isAvatarPresent());
+    }
 }
