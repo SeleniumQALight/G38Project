d9d68baadd73bb15e0c0bbd73d3ddd42888f5d52
qalight <terraco_kiev@ukr.net>
2019-03-05 22:05:23 +0200

    spare

--- overAll/src/main/java/pages/HomePage.java
+++ overAll/src/main/java/pages/HomePage.java
@@ -1,11 +1,18 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage {
+    @FindBy(id = "dictionary")
+    private WebElement menuDictionary;
+
+    @FindBy(id = "spares")
+    private WebElement subMenuSpare;
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -15,5 +22,22 @@ public class HomePage extends ParentPage {
 
     public boolean isAvatarPresent() {
       return actionsWithOurElements.isElementPresent(avatar);
+    }
+
+    public void goToThisPage() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.login("Student","909090");
+        Assert.assertEquals("Avatar is not present"
+                ,true, isAvatarPresent()
+        );
+
+    }
+
+    public void clickOnMenuDictionary(){
+        actionsWithOurElements.clickOnElement(menuDictionary);
+    }
+
+    public void clickOnSubMenuSpare(){
+        actionsWithOurElements.clickOnElement(subMenuSpare);
     }
 }
