911dde5b9b6fbb291e84f50d429994129cfb1bc6
qalight <terraco_kiev@ukr.net>
2019-03-12 21:38:49 +0200

    wait

--- overAll/src/main/java/libs/ActionsWithOurElements.java
+++ overAll/src/main/java/libs/ActionsWithOurElements.java
@@ -5,17 +5,23 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
+    WebDriverWait wait10, wait15;
     public ActionsWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        wait10 = new WebDriverWait(webDriver, 10);
+        wait15 = new WebDriverWait(webDriver, 15);
     }
 
     public void enterTextInToElement(WebElement element, String text){
         try{
+
             element.clear();
             element.sendKeys(text);
             logger.info(text + " was input into element ");
@@ -32,6 +38,10 @@ public class ActionsWithOurElements {
 
     public void clickOnElement(WebElement element) {
         try{
+            wait10.until(ExpectedConditions.elementToBeClickable(element));
+//            wait10.until(ExpectedConditions.not(
+//                    ExpectedConditions.elementToBeClickable(element)
+//            ));
             element.click();
             logger.info("Element was clicked");
         }catch (Exception e){
@@ -88,4 +98,32 @@ public class ActionsWithOurElements {
         }
 
     }
+
+    public void setStatusToCheckBox(WebElement element, String neededState){
+        if ("check".equals(neededState) || "uncheck".equals(neededState)){
+            try{
+                if (element.isSelected() && "check".equals(neededState)){
+                    logger.info("Already check");
+                }else if (!element.isSelected() && "check".equals(neededState)){
+                    element.click();
+                    logger.info("check box checked");
+                }else  if (element.isSelected() && "uncheck".equals(neededState)){
+                    element.click();
+                    logger.info("check box deselected");
+                }else if (!element.isSelected() && "uncheck".equals(neededState)){
+                    logger.info("checkbox is already unchecked");
+                }
+
+            } catch (Exception e){
+                printErrorAndStopTest(e);
+            }
+        } else {
+            logger.error("State should be 'check' or 'uncheck'");
+            Assert.fail("State should be 'check' or 'uncheck'");
+        }
+    }
+
+
+
+
 }
