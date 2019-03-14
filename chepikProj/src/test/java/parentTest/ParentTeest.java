fd7be0496ca815ce04ad8477f1bd857d8ad4f648
Aleksandr <chepik2058@gmail.com>
2019-03-14 21:31:03 +0200

    14.03.19

--- chepikProj/src/test/java/parentTest/ParentTeest.java
+++ chepikProj/src/test/java/parentTest/ParentTeest.java
@@ -6,6 +6,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.EditSparePage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -20,13 +25,11 @@ public class ParentTeest {
     protected HomePage homePage;
     protected SparePage sparePage;
     protected EditSparePage editSparePage;
+    String browser = System.getProperty("browser");
 
     @Before
-    public void setUp () {
-        File file = new File("./src/drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
-        webDriver = new ChromeDriver();
+    public void setUp() {
+        initDriver();
 
         webDriver.manage().window().maximize();//открой окно маккс
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -37,11 +40,42 @@ public class ParentTeest {
         sparePage = new SparePage(webDriver);
         editSparePage = new EditSparePage(webDriver);
     }
+
     @After
     public void tearDown() {
         webDriver.quit();
     }
-    protected void checkExpectedResult (String message, boolean actualResult) {
+
+    protected void checkExpectedResult(String message, boolean actualResult) {
         Assert.assertEquals(message, true, actualResult);
+    }
+
+    private void initDriver() {
+        if (browser == null || "chrome".equals(browser.toLowerCase())) {
+            File file = new File("./src/drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+            webDriver = new ChromeDriver();
+        }else if ("firefox".equals(browser)){
+            File file = new File("./src/drivers/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addPreference("browser.startup.page", 0);
+            profile.addPreference("browser.startup.homepage_override.mstone"
+                    , "ignore"); // Suppress the "What's new" page
+
+
+            webDriver = new FirefoxDriver();
+        }/*else if ("iedriver".equals(browser)) {
+          //  log.info("IE will be started");
+            File file1 = new File("../src/drivers/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            capabilities.setCapability("ignoreZoomSetting", true);
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new InternetExplorerDriver();
+         //   log.info(" IE is started");
+        }*/
     }
 }
