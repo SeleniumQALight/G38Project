702d73dea591172f25d4433d5c2dca059ff0bbc6
t.radulenko <taras.radulenko@idealscorp.com>
2019-03-17 11:03:14 +0200

    API was added

---
+++ overAll/src/main/java/restSteps/demoqa/ApiDemoQA.java
@@ -0,0 +1,23 @@
+package restSteps.demoqa;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBody;
+import org.json.JSONObject;
+import restSteps.ParentApi;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiDemoQA extends ParentApi {
+    public ResponseBody custemerRegistrationByApi(JSONObject requestParams){
+        return given()
+                .contentType(ContentType.JSON)
+                .body(requestParams.toMap())
+                .when()
+                .post(apiEndPoints.restapi_demoqa_com_customer_register())
+                .then()
+                .statusCode(201)
+                .extract()
+                .response().getBody();
+    }
+}
+
