411af87684af7bf1bed6369f4b41488e9543bf48
iboruk <igor.boruk@gmail.com>
2019-03-26 19:29:24 +0200

    Spare Page edit/delete

--- borukProj/src/test/java/parentTest/ParentTest.java
+++ borukProj/src/test/java/parentTest/ParentTest.java
@@ -6,25 +6,38 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import pages.EditSparePage;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.SparePage;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class ParentTest {
-    public WebDriver webDriver = new ChromeDriver();
-    protected LoginPage loginPage = new LoginPage(webDriver);
-    protected HomePage homePage = new HomePage(webDriver);
-    protected SparePage sparePage = new SparePage(webDriver);
-    protected EditSparePage editSparePage = new EditSparePage(webDriver);
+    public WebDriver webDriver;
 
-//
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+    protected SparePage sparePage;
+    protected EditSparePage editSparePage;
+
+    String browser = System.getProperty("browser");
+
+
     @Before
     public void setUp() {
 //        File file = new File("./src/drivers/chromedriver.exe");
 //        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        initDriver();
+        loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
+        sparePage = new SparePage(webDriver);
+        editSparePage = new EditSparePage(webDriver);
+
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -37,5 +50,19 @@ public class ParentTest {
 
     protected void checkExpectedResult (String message, boolean actualResult){
         Assert.assertEquals(message, true, actualResult); // if actualResult = false, than true<>false -> print message
+    }
+
+    private void initDriver(){
+        if (browser == null || "chrome".equals(browser.toLowerCase())) {
+            webDriver = new ChromeDriver();
+        }
+        else if ("firefox".equals(browser)){
+            File file = new File("./src/drivers/geckodriver.exe");
+            System.setProperty ("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addPreference("browser.startup.page",0);
+            profile.addPreference("browser.startup.homepage_overdrive.mstone", "ignore");//Suppress the "What's new" page
+            webDriver = new FirefoxDriver();
+        }
     }
 }
