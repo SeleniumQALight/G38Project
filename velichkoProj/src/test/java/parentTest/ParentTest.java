b36cfe3a35b5bc01eedeb25a12090dde8c274d61
Alina <alinavel85@gmail.com>
2019-03-28 19:34:14 +0200

    File.xl

--- velichkoProj/src/test/java/parentTest/ParentTest.java
+++ velichkoProj/src/test/java/parentTest/ParentTest.java
@@ -1,15 +1,21 @@
 package parentTest;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import pages.EditSparePage;
 import pages.HomePage;
 import pages.LoginPage;
+import pages.SparePage;
 
 import java.io.File;
 import java.sql.Driver;
@@ -17,15 +23,18 @@ import java.util.concurrent.TimeUnit;
 
 public class ParentTest {
     WebDriver webDriver;
+    protected static ConfigProperties  configProperties
+            = ConfigFactory.create(ConfigProperties.class);
     protected LoginPage loginPage;
     protected HomePage homePage;
+    protected SparePage sparePage;
+    protected EditSparePage editSparePage;
+    String browser = System.getProperty("browser");
 
     @Before
     public void setUp() {
+        initDriver();
 
-        File file = new File("./src/drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        webDriver = new ChromeDriver();
 
         //ChromeDriverManager.getInstance().setup();
         //ChromeOptions options = new ChromeOptions();
@@ -35,25 +44,46 @@ public class ParentTest {
         //webDriver = new ChromeDriver(cap);
 
 
-
-
-
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         loginPage = new LoginPage(webDriver);
         homePage = new HomePage(webDriver);
+        sparePage = new SparePage(webDriver);
+        editSparePage = new EditSparePage(webDriver);
+
 
     }
+
     @After
-    public void tearDown(){
+    public void tearDown() {
 
         webDriver.quit();
 
     }
 
-    protected void checkExpectedResult(String message, boolean actualResult){
+    protected void checkExpectedResult(String message, boolean actualResult) {
         Assert.assertEquals(message, true, actualResult);
 
     }
 
+    private void initDriver() {
+        if (browser == null || "chrome".equals(browser.toLowerCase())) {
+
+            File file = new File("./src/drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+            webDriver = new ChromeDriver();
+
+        }else if ("firefox".equals(browser)){
+            File file = new File("./src/drivers/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addPreference("browser.startup.page", 0);
+            profile.addPreference("browser.startup.homepage_override.mstone"
+                    , "ignore"); // Suppress the "What's new" page
+
+
+            webDriver = new FirefoxDriver();
+        }
+
+    }
 }
