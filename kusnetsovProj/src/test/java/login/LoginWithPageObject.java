4e5efacae6e2aceafbce6a26076b7e8a6b87e0bf
Alexandr <kuznietsovv@gmail.com>
2019-03-28 19:34:38 +0200

    Excel

--- kusnetsovProj/src/test/java/login/LoginWithPageObject.java
+++ kusnetsovProj/src/test/java/login/LoginWithPageObject.java
@@ -1,23 +1,24 @@
 package login;
 
-import org.junit.Test;
-import parentTest.ParentTest;
+        import org.junit.Test;
+        import parentTest.ParentTest;
 
 public class LoginWithPageObject extends ParentTest {
     @Test
-    public void validLogin () {
-    loginPage.openPage();
-    loginPage.enterTextInToInputLogin("Student");
-    loginPage.enterTextInToInputPass("909090");
-    loginPage.clickOnButtonSubmit();
+    public void validLogin() {
+        loginPage.openPage();
+        loginPage.enterTextInToInputLogin("Student");
+        loginPage.enterTextInToInputPass("909090");
+        loginPage.clickOnButtonSubmit();
 
-    checkExpectedResult (
-            "Avatar is not present",
-            homePage.isAvatarPresent());
+        checkExpectedResult(
+                "Avatar is not present",
+                homePage.isAvatarPresent());
 
     }
+
     @Test
-    public void invalidLogin(){
+    public void invalidLogin() {
         loginPage.login("tttt", "909090");
         checkExpectedResult("Avatar should not be present",
                 !homePage.isAvatarPresent());
