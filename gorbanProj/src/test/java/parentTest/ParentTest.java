b47187f18e2812e13052e59b81e2d85b502ec8a3
srgorban <46221132+srgorban@users.noreply.github.com>
2019-04-03 19:44:33 +0300

    After lesson 02.04 fix with correct test for unvalidLogin

--- gorbanProj/src/test/java/parentTest/ParentTest.java
+++ gorbanProj/src/test/java/parentTest/ParentTest.java
@@ -1,12 +1,19 @@
 package parentTest;
 
 
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import libs.ConfigProperties;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -25,8 +32,10 @@ import java.util.concurrent.TimeUnit;
 public class ParentTest {
 
     WebDriver webDriver;
-    protected  static ConfigProperties configProperties
+    protected static ConfigProperties configProperties
             = ConfigFactory.create(ConfigProperties.class);
+
+    Logger logger = Logger.getLogger(getClass());
 
     protected LoginPage loginPage;
     protected HomePage homePage;
@@ -51,12 +60,46 @@ public class ParentTest {
 
     @After
     public void tearDown() {
-        webDriver.quit();
+//        webDriver.quit();
     }
 
-    protected void checkExpectedResult(String massage, boolean actualResult) {
-        Assert.assertEquals(massage, actualResult, true);
+    @Step
+    protected void checkExpectedResult(String message, boolean actualResult) {
+        Assert.assertEquals(message, true, actualResult);
     }
+
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        String fileName;
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            screenshot();
+        }
+
+        @Attachment(value = "Page screenshot", type = "image/png")
+        public byte[] saveScreenshot(byte[] screenShot) {
+            return screenShot;
+        }
+
+        public void screenshot() {
+            if (webDriver == null) {
+                logger.info("Driver for screenshot not found");
+                return;
+            }
+            saveScreenshot(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES));
+        }
+
+        @Override
+        protected void finished(Description description) {
+            logger.info(String.format("Finished test: %s::%s", description.getClassName(), description.getMethodName()));
+            try {
+                webDriver.quit();
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+    };
 
     private void initDriver() {
         if (browser == null || "chrome".equals(browser.toLowerCase())) {
@@ -75,7 +118,7 @@ public class ParentTest {
 
 
             webDriver = new FirefoxDriver();
-        }else if ("iedriver".equals(browser)) {
+        } else if ("iedriver".equals(browser)) {
             log.info("IE will be started");
             File file1 = new File("./src/drivers/IEDriverServer.exe");
             System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
