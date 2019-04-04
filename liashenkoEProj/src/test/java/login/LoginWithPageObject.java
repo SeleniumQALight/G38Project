4cb9db7504d0b3ce73f9e52bcc7a0b4ba3a1eb47
LyashenkoEvgen <samuynovuy@ukr.net>
2019-04-04 19:39:44 +0300

    fixx

--- liashenkoEProj/src/test/java/login/LoginWithPageObject.java
+++ liashenkoEProj/src/test/java/login/LoginWithPageObject.java
@@ -1,15 +1,20 @@
 package login;
 
 import libs.ExcelDriver;
+import libs.UtilsForDB;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import parentTest.ParentTest;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class LoginWithPageObject extends ParentTest {
+    UtilsForDB utilsForDB = new UtilsForDB();
     @Test
-    public void validLogin() throws IOException {
+    public void validLogin() throws IOException, SQLException, ClassNotFoundException {
         ExcelDriver excelDriver = new ExcelDriver();
         Map dataForValidLogin = excelDriver.getData(configProperties.DATA_FILE(), "validLogOn");
         loginPages.openPages();
@@ -20,7 +25,14 @@ public class LoginWithPageObject extends ParentTest {
         checkExpectedResult(
                 "Avatar is not present",
                 homePage.isAvatarPresent());
+                utilsForDB.getPassForLogin("Student");
 
+
+
+    }
+    @After
+    public void fd() throws SQLException, IOException, ClassNotFoundException {
+        Assert.assertEquals(1,utilsForDB.deleteElement("vdsvfs"));
     }
 }
 
