ca2101b881a5b5c4f7133ce5788ca6356001a347
LyashenkoEvgen <samuynovuy@ukr.net>
2019-04-04 19:40:10 +0300

    fixx

--- liashenkoEProj/src/main/java/libs/UtilsForDB.java
+++ liashenkoEProj/src/main/java/libs/UtilsForDB.java
@@ -18,9 +18,21 @@ public class UtilsForDB {
         log.info("--- Conected to MySQL --------");
         String tempPass = dBMySQL
                 .selectValue(String.format("select passWord from seleniumTable " +
-                        "where login = '%s')",login));
+                        "where login = '%s'",login));
+        log.info(tempPass);
         dBMySQL.quit();
         return tempPass;
 
+    }
+
+    public int deleteElement(String login) throws SQLException, IOException, ClassNotFoundException {
+        log.info("--- Conect MySQL DB --------");
+        dBMySQL = new Database("MySQL_PADB_DB", "MySQL");
+        log.info("--- Conected to MySQL --------");
+        int tempPass = dBMySQL.changeTable(String.format("select passWord from seleniumTable " +
+                "where login = '%s'",login));
+        log.info(tempPass);
+        dBMySQL.quit();
+        return tempPass;
     }
 }
